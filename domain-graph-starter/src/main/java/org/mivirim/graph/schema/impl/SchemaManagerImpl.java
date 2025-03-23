97dce9e43a88a021d6ceccb8be944936f3958df5
Michael Coddington <michaelcoddington@gmail.com>
2025-03-23 12:23:00 -0400

    Now returns prproperty groups when retrieving an entity schema

--- domain-graph-starter/src/main/java/org/mivirim/graph/schema/impl/SchemaManagerImpl.java
+++ domain-graph-starter/src/main/java/org/mivirim/graph/schema/impl/SchemaManagerImpl.java
@@ -32,7 +32,7 @@ public class SchemaManagerImpl implements SchemaManager {
 
     private static final Logger LOG = LogManager.getLogger(SchemaManagerImpl.class);
 
-    private GraphTraversalSource traversalSource;
+    private final GraphTraversalSource traversalSource;
 
     public SchemaManagerImpl(GraphTraversalSource traversalSource) {
         this.traversalSource = traversalSource;
@@ -52,7 +52,7 @@ public class SchemaManagerImpl implements SchemaManager {
             traversalSource.V().hasLabel(SCHEMA_LABEL).has("name", schema.getName()).next();
             throw new DuplicateException(String.format("Schema %s already exists", schema.getName()));
         } catch (NoSuchElementException er) {
-            GraphTraversal<Vertex, ?> v = traversalSource.addV(SCHEMA_LABEL)
+            GraphTraversal<Vertex, ?> traversal = traversalSource.addV(SCHEMA_LABEL)
                     .property("name", schema.getName()).as("schema");
 
             AtomicInteger vertexCount = new AtomicInteger(0);
@@ -61,36 +61,37 @@ public class SchemaManagerImpl implements SchemaManager {
             for (PropertyDefinition p: schema.getProperties()) {
                 String ref = String.valueOf(vertexCount.getAndIncrement());
 
-                v = v.addV(LabelConstants.SCHEMA_PROPERTY_LABEL)
+                traversal = traversal.addV(LabelConstants.SCHEMA_PROPERTY_LABEL)
                         .property("name", p.getName())
                         .property("type", p.getType().toString())
                         .as(ref);
-                v = v.addE("has-property")
+                traversal = traversal.addE("has-property")
                         .from("schema").to(ref);
             }
 
+            // add property groups
             for (PropertyGroupDefinition groupDefinition: schema.getPropertyGroups()) {
                 String groupRef = String.valueOf(vertexCount.getAndIncrement());
 
-                v = v.addV(LabelConstants.SCHEMA_PROPERTY_GROUP_LABEL)
+                traversal = traversal.addV(LabelConstants.SCHEMA_PROPERTY_GROUP_LABEL)
                         .property("name", groupDefinition.getName())
                         .as(groupRef);
-                v = v.addE("has-property-group")
+                traversal = traversal.addE("has-property-group")
                         .from("schema").to(groupRef);
 
                 for (PropertyDefinition groupPropertyDef: groupDefinition.getProperties()) {
                     String ref = String.valueOf(vertexCount.getAndIncrement());
 
-                    v = v.addV(LabelConstants.SCHEMA_PROPERTY_LABEL)
+                    traversal = traversal.addV(LabelConstants.SCHEMA_PROPERTY_LABEL)
                             .property("name", groupPropertyDef.getName())
                             .property("type", groupPropertyDef.getType().toString())
                             .as(ref);
-                    v = v.addE("has-property")
+                    traversal = traversal.addE("has-property")
                             .from(groupRef).to(ref);
                 }
             }
 
-            v.next();
+            traversal.next();
             LOG.info("Created schema {}", schema);
         } finally {
             tx.commit();
@@ -105,10 +106,14 @@ public class SchemaManagerImpl implements SchemaManager {
 
     @Override
     public Optional<EntityDefinition> retrieveEntitySchema(String name) {
+        var elementProjectionName = "elements";
+        var propertyNodeProjectionName = "propertyNodes";
+        var propertyGroupProjectionName = "propertyGroups";
+
         GraphTraversal<Vertex, Map<String, Object>> iter = traversalSource.V()
                 .hasLabel(SCHEMA_LABEL)
                 .has("name", name)
-                .project("elements", "propertyNodes", "propertyGroupNodes")
+                .project(elementProjectionName, propertyNodeProjectionName, propertyGroupProjectionName)
                 // elements of the schema node
                 .by(__.elementMap())
                 // a list of the property nodes tied to the schema node
@@ -116,7 +121,7 @@ public class SchemaManagerImpl implements SchemaManager {
                 // a map of the property groups tied to the schema and the properties in them
                 .by(
                         __.out("has-property-group")
-                                .project("propertyGroup", "propertyNodes")
+                                .project(elementProjectionName, propertyNodeProjectionName)
                                 .by(__.elementMap())
                                 .by(__.out("has-property").elementMap().fold())
                                 .fold()
@@ -124,25 +129,42 @@ public class SchemaManagerImpl implements SchemaManager {
         if (iter.hasNext()) {
             Map<String, Object> v = iter.next();
             EntityDefinition schema = new EntityDefinition();
-            var propertyMap = (HashMap) v.get("elements");
+            var propertyMap = (HashMap) v.get(elementProjectionName);
             schema.setName((String) propertyMap.get("name"));
 
-            ArrayList<Map<String, Object>> properties = (ArrayList) v.get("propertyNodes");
+            ArrayList<Map<String, Object>> properties = (ArrayList) v.get(propertyNodeProjectionName);
             Set<PropertyDefinition> schemaProps = properties.stream()
-                    .map(map -> {
-                        PropertyDefinition p = new PropertyDefinition();
-                        p.setName((String) map.get("name"));
-                        p.setType(PropertyType.valueOf((String) map.get("type")));
-                        return p;
-                    }).collect(Collectors.toSet());
+                    .map(this::mapToPropertyDefinition).collect(Collectors.toSet());
             schema.setProperties(schemaProps);
 
-            ArrayList<Map<String, Object>> propertyGroups = (ArrayList) v.get("propertyGroupNodes");
+            ArrayList<Map<String, Object>> propertyGroups = (ArrayList) v.get(propertyGroupProjectionName);
+            Set<PropertyGroupDefinition> groups = propertyGroups.stream()
+                    .map(this::mapToPropertyGroupDefinition)
+                    .collect(Collectors.toSet());
+            schema.setPropertyGroups(groups);
 
             return Optional.of(schema);
         } else {
             return Optional.empty();
         }
+    }
+
+    private PropertyDefinition mapToPropertyDefinition(Map<String, Object> map) {
+        PropertyDefinition p = new PropertyDefinition();
+        p.setName((String) map.get("name"));
+        p.setType(PropertyType.valueOf((String) map.get("type")));
+        return p;
+    }
+
+    private PropertyGroupDefinition mapToPropertyGroupDefinition(Map<String, Object> map) {
+        PropertyGroupDefinition group = new PropertyGroupDefinition();
+        Map<String, Object> elements = (Map) map.get("elements");
+        ArrayList<Map<String, Object>> propertyNodes = (ArrayList) map.get("propertyNodes");
+        group.setName((String) elements.get("name"));
+        Set<PropertyDefinition> groupProperties = propertyNodes.stream()
+                .map(this::mapToPropertyDefinition).collect(Collectors.toSet());
+        group.setProperties(groupProperties);
+        return group;
     }
 
     @Override
